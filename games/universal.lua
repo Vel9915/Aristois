cc60cff27453c3ab00b273e1238d62861c342f16
real pet sim x yotuber <131480494+Vel9915@users.noreply.github.com>
2023-07-16 10:19:59 +0100

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -913,7 +913,7 @@ end
     local KillauraShowTarget = {}
     local KillauraMulti = {}
     local HitRemote = Client:Get(remotes.SwordRemote)
-    local Killaura = {}; Killaura = GuiLibrary.Objects.combatWindow.API.CreateOptionsButton({
+    local Killaura = {}; Killaura = GuiLibrary.Objects.exploitsWindow.API.CreateOptionsButton({
         Name = "killaura",
         Function = function(callback) 
             if callback then 
